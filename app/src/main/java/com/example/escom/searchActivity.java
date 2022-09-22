774638084ebdf80632664253ad6f2725b2136bc8
LaraBusyanil <89774038+LaraBusyanil@users.noreply.github.com>
2022-09-22 08:14:47 +0700

    tes7

---
+++ app/src/main/java/com/example/escom/searchActivity.java
@@ -0,0 +1,18 @@
+package com.example.escom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class searchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+
+    public void back(View view) {
+    }
+}
