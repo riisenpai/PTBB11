95b33e71df51518030ab0fcb0289101d4f7b40a6
Reysha28 <irsyalinareysha@gmail.com>
2022-12-07 21:19:31 +0700

    rev 19

--- app/src/main/java/com/example/escom/HomeActivity.java
+++ app/src/main/java/com/example/escom/HomeActivity.java
@@ -26,6 +26,7 @@ public class HomeActivity extends AppCompatActivity {
     private NotificationManagerCompat notificationManagerMhsTA;
     private Button buttonShow;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
